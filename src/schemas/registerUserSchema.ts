de3c9a8324b78ae0ab2fb30dbb3c6765d3b29022
lucasfranchini <lucasfranchini.a@gmail.com>
2021-08-05 18:41:57 -0300

    correct registerUserSchema

--- src/schemas/registerUserSchema.ts
+++ src/schemas/registerUserSchema.ts
@@ -5,6 +5,6 @@ const registerUserSchema = Joi.object({
     email: Joi.string().email().required(),
     password: Joi.string().min(3).required(),
     confirmPassword: Joi.ref('password')
-})
+}).with('password', 'confirmPassword');
 
 export default registerUserSchema;
