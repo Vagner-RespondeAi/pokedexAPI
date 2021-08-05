6ccc038cac085e794f52faeab41882e005fdb677
lucasfranchini <lucasfranchini.a@gmail.com>
2021-08-05 18:41:37 -0300

    correct validation in sign in

--- src/controllers/userConroller.ts
+++ src/controllers/userConroller.ts
@@ -3,6 +3,7 @@ import registerUserSchema from "../schemas/registerUserSchema";
 import NewUser from "../interfaces/newUser";
 import * as userService from "../services/userService";
 import User from "../entities/User";
+import userSchema from "../schemas/userSchema";
 
 
 export async function signUp(req:Request,res:Response){
@@ -14,9 +15,9 @@ export async function signUp(req:Request,res:Response){
 }
 
 export async function signIn(req:Request,res:Response){
-  const newUser = req.body as User;
-  if(registerUserSchema.validate(newUser).error) return res.sendStatus(400)
-  const token = await userService.signIn(req.body);
+  const user = req.body as User;
+  if(userSchema.validate(user).error) return res.sendStatus(400)
+  const token = await userService.signIn(user);
   if(!token) res.sendStatus(401)
   res.send({token})
 }
