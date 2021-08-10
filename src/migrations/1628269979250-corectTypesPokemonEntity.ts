153288ae0efd7822d8275b9dd054172049b523a0
lucasfranchini <lucasfranchini.a@gmail.com>
2021-08-10 17:11:00 -0300

    style: Format migrations folder

--- src/migrations/1628269979250-corectTypesPokemonEntity.ts
+++ src/migrations/1628269979250-corectTypesPokemonEntity.ts
@@ -1,24 +1,37 @@
-import {MigrationInterface, QueryRunner} from "typeorm";
+import { MigrationInterface, QueryRunner } from "typeorm";
 
-export class corectTypesPokemonEntity1628269979250 implements MigrationInterface {
-    name = 'corectTypesPokemonEntity1628269979250'
+export class corectTypesPokemonEntity1628269979250
+  implements MigrationInterface
+{
+  name = "corectTypesPokemonEntity1628269979250";
 
-    public async up(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`ALTER TABLE "pokemons" DROP COLUMN "weight"`);
-        await queryRunner.query(`ALTER TABLE "pokemons" ADD "weight" integer NOT NULL`);
-        await queryRunner.query(`ALTER TABLE "pokemons" DROP COLUMN "height"`);
-        await queryRunner.query(`ALTER TABLE "pokemons" ADD "height" integer NOT NULL`);
-        await queryRunner.query(`ALTER TABLE "pokemons" DROP COLUMN "baseExp"`);
-        await queryRunner.query(`ALTER TABLE "pokemons" ADD "baseExp" integer NOT NULL`);
-    }
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(`ALTER TABLE "pokemons" DROP COLUMN "weight"`);
+    await queryRunner.query(
+      `ALTER TABLE "pokemons" ADD "weight" integer NOT NULL`
+    );
+    await queryRunner.query(`ALTER TABLE "pokemons" DROP COLUMN "height"`);
+    await queryRunner.query(
+      `ALTER TABLE "pokemons" ADD "height" integer NOT NULL`
+    );
+    await queryRunner.query(`ALTER TABLE "pokemons" DROP COLUMN "baseExp"`);
+    await queryRunner.query(
+      `ALTER TABLE "pokemons" ADD "baseExp" integer NOT NULL`
+    );
+  }
 
-    public async down(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`ALTER TABLE "pokemons" DROP COLUMN "baseExp"`);
-        await queryRunner.query(`ALTER TABLE "pokemons" ADD "baseExp" character varying NOT NULL`);
-        await queryRunner.query(`ALTER TABLE "pokemons" DROP COLUMN "height"`);
-        await queryRunner.query(`ALTER TABLE "pokemons" ADD "height" character varying NOT NULL`);
-        await queryRunner.query(`ALTER TABLE "pokemons" DROP COLUMN "weight"`);
-        await queryRunner.query(`ALTER TABLE "pokemons" ADD "weight" character varying NOT NULL`);
-    }
-
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(`ALTER TABLE "pokemons" DROP COLUMN "baseExp"`);
+    await queryRunner.query(
+      `ALTER TABLE "pokemons" ADD "baseExp" character varying NOT NULL`
+    );
+    await queryRunner.query(`ALTER TABLE "pokemons" DROP COLUMN "height"`);
+    await queryRunner.query(
+      `ALTER TABLE "pokemons" ADD "height" character varying NOT NULL`
+    );
+    await queryRunner.query(`ALTER TABLE "pokemons" DROP COLUMN "weight"`);
+    await queryRunner.query(
+      `ALTER TABLE "pokemons" ADD "weight" character varying NOT NULL`
+    );
+  }
 }
