6aa489ddfbfcd598191839b35ef2b9768673400b
lucasfranchini <lucasfranchini.a@gmail.com>
2021-08-10 17:13:42 -0300

    style: Format controller folder

--- src/controllers/userController.ts
+++ src/controllers/userController.ts
@@ -5,29 +5,26 @@ import * as userService from "../services/userService";
 import User from "../entities/User";
 import userSchema from "../schemas/userSchema";
 
-
-export async function signUp(req:Request,res:Response,next:NextFunction){
-  try{
-    const newUser = req.body as NewUser
-    if(registerUserSchema.validate(newUser).error) return res.sendStatus(400)
-    const result = await userService.signUp(newUser)
-    if(!result) return res.sendStatus(409)
-    return res.sendStatus(201)
-  }
-  catch(e){
-    next(e)
+export async function signUp(req: Request, res: Response, next: NextFunction) {
+  try {
+    const newUser = req.body as NewUser;
+    if (registerUserSchema.validate(newUser).error) return res.sendStatus(400);
+    const result = await userService.signUp(newUser);
+    if (!result) return res.sendStatus(409);
+    return res.sendStatus(201);
+  } catch (e) {
+    next(e);
   }
 }
 
-export async function signIn(req:Request,res:Response,next:NextFunction){
-  try{
+export async function signIn(req: Request, res: Response, next: NextFunction) {
+  try {
     const user = req.body as User;
-    if(userSchema.validate(user).error) return res.sendStatus(400)
+    if (userSchema.validate(user).error) return res.sendStatus(400);
     const token = await userService.signIn(user);
-    if(!token) return res.sendStatus(401)
-    res.send({token})
-  }
-  catch(e){
-    next(e)
+    if (!token) return res.sendStatus(401);
+    res.send({ token });
+  } catch (e) {
+    next(e);
   }
 }
