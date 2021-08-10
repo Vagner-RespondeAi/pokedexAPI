2a7a2364971e2f7728e0e47808355185ec214499
lucasfranchini <lucasfranchini.a@gmail.com>
2021-08-10 17:10:36 -0300

    style: Format schemas folder

--- src/schemas/registerUserSchema.ts
+++ src/schemas/registerUserSchema.ts
@@ -1,10 +1,9 @@
 import Joi from "joi";
 
-
 const registerUserSchema = Joi.object({
-    email: Joi.string().email().required(),
-    password: Joi.string().min(3).required(),
-    confirmPassword: Joi.ref('password')
-}).with('password', 'confirmPassword');
+  email: Joi.string().email().required(),
+  password: Joi.string().min(3).required(),
+  confirmPassword: Joi.ref("password"),
+}).with("password", "confirmPassword");
 
 export default registerUserSchema;
--- src/schemas/userSchema.ts
+++ src/schemas/userSchema.ts
@@ -1,9 +1,8 @@
 import Joi from "joi";
 
-
 const userSchema = Joi.object({
-    email: Joi.string().email().required(),
-    password: Joi.string().min(3).required()
-})
+  email: Joi.string().email().required(),
+  password: Joi.string().min(3).required(),
+});
 
 export default userSchema;
