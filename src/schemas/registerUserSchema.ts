57e37496317ebae48c881667578211575d304f04
vagnerpavani <vagner.alves.pavani@gmail.com>
2021-08-13 16:19:39 -0300

    preparando review

--- src/schemas/registerUserSchema.ts
+++
@@ -1,9 +0,0 @@
-import Joi from "joi";
-
-const registerUserSchema = Joi.object({
-  email: Joi.string().email().required(),
-  password: Joi.string().min(3).required(),
-  confirmPassword: Joi.ref("password"),
-}).with("password", "confirmPassword");
-
-export default registerUserSchema;
